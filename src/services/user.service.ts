a1c659ec87ce7b6aa3551c49a25804e3d090efb8
Arthur Cardoso <arthurthorp@hotmail.com>
2026-03-31 14:29:41 -0300

    feat: add createUser mutation

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,24 @@
 import { prisma } from "../../prisma/prisma";
+import { CreateUserInput } from "../dtos/input/user.input";
 
 export class UserService {
+  async createUser(data: CreateUserInput) {
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (findUser) throw new Error("User already registered");
+
+    return prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+      },
+    });
+  }
+
   async findUser(id: string) {
     const user = await prisma.user.findUnique({
       where: { id },
